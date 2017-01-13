0c376bcb20d1d6a5b5a59b366bb1c114b86c47b6
Mike Bajur <mbajur@gmail.com>
2017-01-13 11:10:06 +0100

    Fix datetime picker translations, closes #24

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require moment.min
+//= require moment.pl
 //= require bootstrap-datetimepicker.min
 //= require google-analytics-turbolinks
 //= require Luminous.min
