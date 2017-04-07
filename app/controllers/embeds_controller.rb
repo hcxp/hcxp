02e61de0c3fdba49d37c89be0025ca07ffc40ff3
Michal Bajur <mbajur@gmail.com>
2017-04-07 12:49:07 +0200

    Disable overflow in embeds + add utm info in embed links

--- app/controllers/embeds_controller.rb
+++ app/controllers/embeds_controller.rb
@@ -11,6 +11,9 @@ class EmbedsController < ApplicationController
     @poster_height    = params[:poster_height] || 150
     @accent_color     = params[:accent_color] || '#e45735'
     @background_color = params[:background_color] || '#ffffff'
+
+    @disable_horizontal_scroll = params[:disable_horizontal_scroll].present?
+    @disable_vertical_scroll   = params[:disable_vertical_scroll].present?
   end
 
   private
